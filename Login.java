0b22fd6563648b2b50adf62c6424e90c8dc5c210
Ivan <grabovivan@gmailcom>
2022-10-25 10:16:30 +0300

    Login class added

---
+++ Login.java
@@ -0,0 +1,77 @@
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+public class Login extends JFrame{
+    private JPanel contentPane;
+    private JTextField firstname;
+    private JTextField lastname;
+    private JTextField username_mobile;
+    private JPasswordField passwordField;
+    private JButton loginButton;
+
+    public static void main(String[] args){
+        try {
+            Login frame = new Login();
+            frame.setSize(550, 870);
+            frame.setVisible(true);
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+    };
+
+    public Login(){
+        setIconImage(Toolkit.getDefaultToolkit().getImage("bank_icon.png"));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(450, 190, 550, 870);
+        setResizable(false);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+        JLabel lblUserLogin = new JLabel("Welcome to TinkOn bank");
+        lblUserLogin.setFont(new Font("Times New Roman", Font.PLAIN, 42));
+        lblUserLogin.setBounds(45, 52, 460, 50);
+        contentPane.add(lblUserLogin);
+
+        JLabel lblEmailAddress = new JLabel("Username or Mobile");
+        lblEmailAddress.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        lblEmailAddress.setBounds(58, 250, 250, 36);
+        contentPane.add(lblEmailAddress);
+
+        username_mobile = new JTextField();
+        username_mobile.setFont(new Font("Tahoma", Font.PLAIN, 32));
+        username_mobile.setBounds(260, 245, 228, 50);
+        contentPane.add(username_mobile);
+
+        JLabel lblPassword = new JLabel("Password");
+        lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        lblPassword.setBounds(58, 330, 99, 24);
+        contentPane.add(lblPassword);
+
+        passwordField = new JPasswordField();
+        passwordField.setFont(new Font("Tahoma", Font.PLAIN, 32));
+        passwordField.setBounds(260, 320, 228, 50);
+        contentPane.add(passwordField);
+
+        loginButton = new JButton("Login");
+        loginButton.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        loginButton.setBounds(170, 700, 200, 74);
+        contentPane.add(loginButton);
+    }
+}
--- Registration.java
+++ Registration.java
@@ -1,31 +1,55 @@
-import java.awt.*;
-
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 
-public class Registration{
+public class Registration extends JFrame {
+    private JPanel contentPane;
+    private JTextField firstname;
+    private JTextField lastname;
+    private JTextField email;
+    private JTextField username;
+    private JTextField mobile;
+    private JPasswordField passwordField;
+    private JButton btnNewButton;
+
     public static void main(String[] args) {
+        try {
+            Registration frame = new Registration();
+            frame.setSize(550, 870);
+            frame.setVisible(true);
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+    };
 
-        JFrame MainFrame = new JFrame("TinkOn Bank");
-        MainFrame.setVisible(true);
-        MainFrame.setIconImage(Toolkit.getDefaultToolkit().getImage("bank_icon.png"));
-        MainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        MainFrame.setBounds(450, 190, 1014, 597);
-        MainFrame.setResizable(false);
-
-        JPanel contentPane = new JPanel();
+    public Registration() {
+        setIconImage(Toolkit.getDefaultToolkit().getImage("bank_icon.png"));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(450, 190, 550, 870);
+        setResizable(false);
+        contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        MainFrame.setContentPane(contentPane);
+        setContentPane(contentPane);
         contentPane.setLayout(null);
 
-        JLabel lblNewUserRegister = new JLabel("New User Register");
+        JLabel lblNewUserRegister = new JLabel("Welcome to TinkOn bank");
         lblNewUserRegister.setFont(new Font("Times New Roman", Font.PLAIN, 42));
-        lblNewUserRegister.setBounds(362, 52, 325, 50);
+        lblNewUserRegister.setBounds(45, 52, 460, 50);
         contentPane.add(lblNewUserRegister);
 
         JLabel lblName = new JLabel("First name");
@@ -43,59 +67,54 @@ public class Registration{
         lblEmailAddress.setBounds(58, 324, 124, 36);
         contentPane.add(lblEmailAddress);
 
-        JTextField firstname = new JTextField();
+        firstname = new JTextField();
         firstname.setFont(new Font("Tahoma", Font.PLAIN, 32));
         firstname.setBounds(214, 151, 228, 50);
         contentPane.add(firstname);
-        firstname.setColumns(10);
 
-        JTextField lastname = new JTextField();
+        lastname = new JTextField();
         lastname.setFont(new Font("Tahoma", Font.PLAIN, 32));
         lastname.setBounds(214, 235, 228, 50);
         contentPane.add(lastname);
-        lastname.setColumns(10);
 
-        JTextField email = new JTextField();
+        email = new JTextField();
         email.setFont(new Font("Tahoma", Font.PLAIN, 32));
         email.setBounds(214, 320, 228, 50);
         contentPane.add(email);
-        email.setColumns(10);
 
-        JTextField username = new JTextField();
+        username = new JTextField();
         username.setFont(new Font("Tahoma", Font.PLAIN, 32));
-        username.setBounds(707, 151, 228, 50);
+        username.setBounds(214, 397, 228, 50);
         contentPane.add(username);
-        username.setColumns(10);
 
         JLabel lblUsername = new JLabel("Username");
         lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 20));
-        lblUsername.setBounds(542, 159, 99, 29);
+        lblUsername.setBounds(58, 405, 99, 29);
         contentPane.add(lblUsername);
 
         JLabel lblPassword = new JLabel("Password");
         lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 20));
-        lblPassword.setBounds(542, 245, 99, 24);
+        lblPassword.setBounds(58, 557, 99, 24);
         contentPane.add(lblPassword);
 
         JLabel lblMobileNumber = new JLabel("Mobile number");
         lblMobileNumber.setFont(new Font("Tahoma", Font.PLAIN, 20));
-        lblMobileNumber.setBounds(542, 329, 139, 26);
+        lblMobileNumber.setBounds(58, 478, 139, 26);
         contentPane.add(lblMobileNumber);
 
-        JTextField mobile = new JTextField();
+        mobile = new JTextField();
         mobile.setFont(new Font("Tahoma", Font.PLAIN, 32));
-        mobile.setBounds(707, 320, 228, 50);
+        mobile.setBounds(214, 472, 228, 50);
         contentPane.add(mobile);
-        mobile.setColumns(10);
 
-        JTextField passwordField = new JPasswordField();
+        passwordField = new JPasswordField();
         passwordField.setFont(new Font("Tahoma", Font.PLAIN, 32));
-        passwordField.setBounds(707, 235, 228, 50);
+        passwordField.setBounds(214, 547, 228, 50);
         contentPane.add(passwordField);
 
-        JButton btnNewButton = new JButton("Register");
-        btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 32));
-        btnNewButton.setBounds(407, 400, 228, 50);
+        btnNewButton = new JButton("Register");
+        btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        btnNewButton.setBounds(140, 700, 259, 74);
         contentPane.add(btnNewButton);
-        }
+    }
 }
